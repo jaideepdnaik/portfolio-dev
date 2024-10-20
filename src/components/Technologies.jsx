fa262873f9bede5276f5a9c29b3c2cd86e9b8d3d
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-20 22:19:38 +0530

    Updated the color boldness.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -15,34 +15,34 @@ const Technologies = () => {
       <h2 className="my-20 text-center text-4xl">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <RiReactjsLine className="text-4xl text-cyan-400"></RiReactjsLine>
+            <RiReactjsLine className="text-4xl text-cyan-500"></RiReactjsLine>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiMongodb className="text-4xl text-green-400"></SiMongodb>
+            <SiMongodb className="text-4xl text-green-500"></SiMongodb>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <DiRedis className="text-4xl text-red-400"></DiRedis>
+            <DiRedis className="text-4xl text-red-700"></DiRedis>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaNodeJs className="text-4xl text-green-400"></FaNodeJs>
+            <FaNodeJs className="text-4xl text-green-500"></FaNodeJs>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <BiLogoPostgresql className="text-4xl text-blue-400"></BiLogoPostgresql>
+            <BiLogoPostgresql className="text-4xl text-blue-800"></BiLogoPostgresql>
         </div>  
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <DiDotnet className="text-4xl text-white-400"></DiDotnet>
+            <DiDotnet className="text-4xl text-white-500"></DiDotnet>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiExpress className="text-4xl text-white-400"></SiExpress>
+            <SiExpress className="text-4xl text-white-600"></SiExpress>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <DiDocker className="text-4xl text-blue-400"></DiDocker>
+            <DiDocker className="text-4xl text-blue-500"></DiDocker>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
             <BsGithub className="text-4xl text-white-400"></BsGithub>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiSocketdotio className="text-4xl text-blue-400"></SiSocketdotio>
+            <SiSocketdotio className="text-4xl text-blue-600"></SiSocketdotio>
         </div>  
       </div>
     </div>
