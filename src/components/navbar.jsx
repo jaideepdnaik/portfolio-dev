e08feac332d1dc89e827361c09c701c126b63589
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-20 14:36:17 +0530

    Removed alt from img tag.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
     return <div className="bg-black-800 mb-20 flex item-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="" width="75" height="30"/>
+            <img src={logo} width="75" height="30"/>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedin></FaLinkedin>
