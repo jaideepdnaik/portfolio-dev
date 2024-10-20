e33b76e604f9ecdb51da8998f422161eb3914378
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-20 11:35:36 +0530

    Imported the styling in navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,11 +2,12 @@ import logo from "../assets/Logo.png";
 import { FaLinkedin } from "react-icons/fa";  
 import { FaGithub } from "react-icons/fa";  
 import { FaInstagram } from "react-icons/fa";  
+import './styles.css';  
 
 const Navbar = () => {
     return <div className="bg-black-800 mb-20 flex item-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="" width="60" height="30"/>
+            <img src={logo} alt="" width="50" height="30" className="logo"/>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedin></FaLinkedin>
