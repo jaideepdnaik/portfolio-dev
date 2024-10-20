6da5bffd9dac0f311899331d2b6e9cebebd31b9d
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-20 14:39:19 +0530

    Increased the size on the social media icons.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="flex flex-shrink-0 items-center">
             <img src={logo} width="75" height="30"/>
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <div className="m-8 flex items-center justify-center gap-4 text-lg">
             <FaLinkedin></FaLinkedin>
             <FaGithub></FaGithub>
             <FaInstagram></FaInstagram>
