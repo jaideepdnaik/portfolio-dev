3a939b4894772ad78dff97b15b3286d224866996
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-20 15:39:06 +0530

    Imported and used the Hero component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -7,6 +7,7 @@ const App = () => {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container mx-auto">
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
