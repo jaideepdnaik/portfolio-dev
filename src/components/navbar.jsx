c142fa62218e6286b3b7535550c138a413f224da
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-20 10:33:32 +0530

    Imported the Logo in navbar.jsx
    Also updated the Navbar component to be used in app.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,11 @@
+import logo from "../assets/Logo.png";  
+
 const Navbar = () => {
-    return <div>Navbar</div>
+    return <div className="bg-black-800 mb-20 flex item-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <img src={logo} alt="" />
+        </div>
+    </div>
 }
 
 export default Navbar;
