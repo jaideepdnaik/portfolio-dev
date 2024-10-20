6229b14cf0107935a901e3d28da540158cdb0126
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-20 10:54:06 +0530

    Added Social Media Logos.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,17 @@
-import logo from "../assets/Logo.png";  
+import logo from "../assets/Logo.png";
+import { FaLinkedin } from "react-icons/fa";  
+import { FaGithub } from "react-icons/fa";  
+import { FaInstagram } from "react-icons/fa";  
 
 const Navbar = () => {
     return <div className="bg-black-800 mb-20 flex item-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="" />
+            <img src={logo} alt="" width="60" height="30"/>
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin></FaLinkedin>
+            <FaGithub></FaGithub>
+            <FaInstagram></FaInstagram>
         </div>
     </div>
 }
