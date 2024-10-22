49a49a2abdd7e938d3bae50a26af6e5806c256f0
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-22 22:31:21 +0530

    Updated the About.jsx for responsiveness
    As per the YouTube video.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,24 +3,24 @@ import { ABOUT_TEXT } from "../constants";
 
 const About = () => {
   return (
-    <div className="border-b border-neutral-900 pb-2">
-      <h2 className="my-20 text-center text-4xl mb-16">
+    <div className="border-b border-neutral-900 pb-8">
+      <h2 className="my-20 text-center text-4xl mb-8">
         About
         <span className="text-neutral-500"> Me</span>
       </h2>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
-          <div className="flex items-center justify-center mb-16">
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex items-center justify-center">
             <img
               src={aboutImage}
               alt="About Picture"
-              className="w-[290px] h-[350px] lg:ml-14 rounded-2xl"
+              className="w-[290px] h-[350px] lg:ml-60 rounded-2xl"
             />
           </div>
         </div>
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center lg:justify-start">
-            <p className="text-sm mx-4 text-justify max-w-xl font-light tracking-tighter">{ABOUT_TEXT}</p>
+            <p className="my-2 mx-4 text-justify max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
         </div>
       </div>
