8bdd169253a536d3f0abd397bc5615510524d56b
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-22 22:40:01 +0530

    Made some changes in Hero.jsx
    But not complete. Have to update yet. Its pending.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,13 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-4 mx-4 text-6xl font-thin tracking-tight lg:text-5-xl">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">
               Jaideep D Naik
             </h1>
-            <span className="mx-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent">
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Backend Developer
             </span>
-            <p className="my-2 mb-6 mx-4 text-justify text-sm py-2 max-w-xl font-light tracking-tighter">
+            <p className="my-2 text-justify text-sm py-6 max-w-xl font-light tracking-tighter">
               {HERO_CONTENT}
             </p>
           </div>
