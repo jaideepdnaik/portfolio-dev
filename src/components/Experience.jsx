7395a8fbdef1ba93f6245b6928179f368e003ae8
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-22 21:30:55 +0530

    Created and imported the Experience component.

---
+++ src/components/Experience.jsx
@@ -0,0 +1,9 @@
+const Experience = () => {
+    return (
+        <div className="border-b border-neutral-900 pb-4">
+            <h2 className="my-20 text-center text-4xl">Experience</h2>
+        </div>
+    )
+};
+
+export default Experience;
