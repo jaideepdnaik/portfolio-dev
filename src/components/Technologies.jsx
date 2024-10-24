c1e86d57256ba6c823f6daa27a964db7cfe78540
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-24 22:42:32 +0530

    Added the motion feature for technologies section.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,43 +8,127 @@ import { SiExpress } from "react-icons/si";
 import { DiDocker } from "react-icons/di";
 import { BsGithub } from "react-icons/bs";
 import { SiSocketdotio } from "react-icons/si";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <RiReactjsLine className="text-6xl text-cyan-500"></RiReactjsLine>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiMongodb className="text-6xl text-green-500"></SiMongodb>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <DiRedis className="text-6xl text-red-700"></DiRedis>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaNodeJs className="text-6xl text-green-500"></FaNodeJs>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <BiLogoPostgresql className="text-6xl text-blue-800"></BiLogoPostgresql>
-        </div>  
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <DiDotnet className="text-6xl text-white-500"></DiDotnet>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiExpress className="text-6xl text-white-600"></SiExpress>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <DiDocker className="text-6xl text-blue-500"></DiDocker>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <BsGithub className="text-6xl text-white-400"></BsGithub>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiSocketdotio className="text-6xl text-blue-600"></SiSocketdotio>
-        </div>  
-      </div>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-6xl text-cyan-500"></RiReactjsLine>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2.2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-6xl text-green-500"></SiMongodb>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4.3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiRedis className="text-6xl text-red-700"></DiRedis>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2.4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-6xl text-green-500"></FaNodeJs>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3.8)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoPostgresql className="text-6xl text-blue-800"></BiLogoPostgresql>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiDotnet className="text-6xl text-white-500"></DiDotnet>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiExpress className="text-6xl text-white-600"></SiExpress>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiDocker className="text-6xl text-blue-500"></DiDocker>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3.0)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BsGithub className="text-6xl text-white-400"></BsGithub>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiSocketdotio className="text-6xl text-blue-600"></SiSocketdotio>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
