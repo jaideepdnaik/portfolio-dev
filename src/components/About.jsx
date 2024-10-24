f77c70de254b815c43550909c0feb5812f1ed7f4
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-24 22:21:54 +0530

    Added motion to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import aboutImage from "../assets/About_Picture.jpg";
 import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
@@ -9,7 +10,12 @@ const About = () => {
         <span className="text-neutral-500"> Me</span>
       </h2>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
           <div className="flex items-center justify-center">
             <img
               src={aboutImage}
@@ -17,12 +23,19 @@ const About = () => {
               className="w-[290px] h-[350px] lg:ml-60 rounded-2xl"
             />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
           <div className="flex justify-center lg:justify-start">
-            <p className="my-2 mx-6 font-light text-justify max-w-xl py-6">{ABOUT_TEXT}</p>
+            <p className="my-2 mx-6 font-light text-justify max-w-xl py-6">
+              {ABOUT_TEXT}
+            </p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
