21527f45e8562c29e4c46b6577ac668ab72692c4
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-10-29 22:30:20 +0530

    Added href's for LinkedIn, Github and Instagram.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,16 +4,36 @@ import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-    return <div className="mb-12 flex item-center justify-between py-6 lg:mx-40 mt-4">
-        <div className="flex flex-shrink-0 items-center">
-            <img src={logo} width="75" height="30"/>
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-lg">
-            <FaLinkedin></FaLinkedin>
-            <FaGithub></FaGithub>
-            <FaInstagram></FaInstagram>
-        </div>
+  return (
+    <div className="mb-12 flex item-center justify-between py-6 lg:mx-40 mt-4">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} width="75" height="30" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-lg">
+        <a
+          href="https://www.linkedin.com/in/jaideep-d-naik-405335205/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin></FaLinkedin>
+        </a>
+        <a
+          href="https://github.com/jaideepdnaik"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub></FaGithub>
+        </a>
+        <a
+          href="https://www.instagram.com/jaideep_d_naik/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram></FaInstagram>
+        </a>
+      </div>
     </div>
-}
+  );
+};
 
 export default Navbar;
